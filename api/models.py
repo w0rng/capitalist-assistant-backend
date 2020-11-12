f915ec9f7ae88bed720721dc3837bc557a8f516b
kiri11_mi1 <kirill.mil.0310@mail.ru>
2020-11-12 17:54:12 +0700

    создал модель кредита

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Credit(models.Model):
+    name = models.TextField(verbose_name='Название кредита')
+    value = models.FloatField(verbose_name='Величина кредита')
+    payment = models.FloatField(verbose_name='Ежемесячная выплата')
+
+
+    def __str__(self):
+        return f'{self.name}\t{self.value}\t{self.payment}'
+
+
+    class Meta:
+        verbose_name = u'Кредит'
+        verbose_name_plural = u'Кредиты'
