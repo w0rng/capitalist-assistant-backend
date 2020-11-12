82bf62b1f600855b362832ab1320143a7f1972db
Bekzod Buyukov <bek.buyukov@gmail.com>
2020-11-13 05:14:29 +0700

    Модели зарегистрированы, проведена миграция

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Transaction, Contribution
 
 # Register your models here.
+
+admin.site.register(Transaction)
+admin.site.register(Contribution)
