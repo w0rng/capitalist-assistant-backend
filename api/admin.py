4e00c50bff834ad0253605191f0982656b71d7b7
w0rng <antonabramov2000@gmail.com>
2020-11-13 14:19:28 +0700

    Отрефакторил регистрацию моделей

--- api/admin.py
+++ api/admin.py
@@ -5,12 +5,25 @@ from random import randint
 
 @admin.register(models.User)
 class User(admin.ModelAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'date_joined', 'get_token', 'age', 'gender')
+    list_display = ('get_email', 'first_name', 'last_name', 'date_joined', 'get_token', 'age', 'gender')
 
     def get_token(self, obj):
         return obj.token[:5] + '*'*randint(3, 15) + obj.token[-5:]
 
+    def get_email(self, obj):
+        i = obj.email.index('@')
+        return obj.email[:3] + '*'*randint(2, 7) + obj.email[i:]
 
-admin.site.register(models.Credit)
-admin.site.register(models.Transaction)
-admin.site.register(models.Contribution)
+
+@admin.register(models.Credit)
+class Credit(admin.ModelAdmin):
+    list_display = ('name', 'value', 'payment', 'user_id')
+
+
+@admin.register(models.Contribution)
+class Contribution(admin.ModelAdmin):
+    list_display = ('name', 'percentage_rate', 'current_amount', 'percentage_accrual_date', 'percentage_to_contribution', 'user_id')
+
+@admin.register(models.Transaction)
+class Transaction(admin.ModelAdmin):
+    list_display = ('name', 'ticket', 'price', 'data', 'active_type', 'user_id')
