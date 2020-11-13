d8757963b894683ef869424160f2a4edb8314843
w0rng <antonabramov2000@gmail.com>
2020-11-13 12:40:01 +0700

    Регистрация модели и включение менеджера

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+import api.models as models
+from random import randint
 
-# Register your models here.
+
+@admin.register(models.User)
+class User(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'date_joined', 'get_token', 'age', 'gender')
+
+    def get_token(self, obj):
+        return obj.token[:5] + '*'*randint(3, 15) + obj.token[-5:]
